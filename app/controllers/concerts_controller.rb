2bf8fb74438dd95a0daf9f33e7f9a08afe7b02ec
jfanzago <jfanzago@gmail.com>
2016-02-08 09:59:51 -0500

    concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,13 +1,31 @@
 class ConcertsController < ApplicationController
   def index
+  	@concert = Concert.all
   end
 
   def show
+  	@concert
   end
 
   def create
+  		@concert = Concert.new concert_params
+  		if @concert.save
+  			redirect_to concerts_path(@concert)
+  		else 
+  			render 'new'
+ 	end
   end
 
   def new
+  		@concert = Concert.new
+
   end
+
+  private
+  def concert_params
+ 	 params.require(:concert).permit(:date, :name, :price, :venue, :city, :description)
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'concerts/index'
+  # get 'concerts/index'
 
-  get 'concerts/show'
+  # get 'concerts/show'
 
-  get 'concerts/create'
+  # get 'concerts/create'
 
-  get 'concerts/new'
+  # get 'concerts/new'
 
-
+resources :concerts, only: [:index, :show, :create, :new]
+  
 end
